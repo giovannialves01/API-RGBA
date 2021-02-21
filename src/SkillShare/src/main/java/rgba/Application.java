ba3866202f79a467b8988d6ecf53912f84e76fe9
Rafael Furtado <rafs.97@hotmail.com>
2021-02-21 14:58:15 -0300

    Correções para gerar .jar adequadamente
    Para gerar um .jar utilizável do projeto e enviar para o servidor da Azure, utilize "gradlew bootJar" no prompt de comando

--- src/SkillShare/src/main/java/rgba/Application.java
+++ src/SkillShare/src/main/java/rgba/Application.java
@@ -1,14 +1,20 @@
 package rgba;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class Application {
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		
+		new SpringApplicationBuilder(Application.class).build().run(args);
 	}
 
 }
