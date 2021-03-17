b090e80f9d2612bbfd339d12ae8562e018a5018f
NicholasRoque <nicholas.sroque@gmail.com>
2021-03-17 18:41:35 -0300

    update javadocs and add swagger

--- src/SkillShare/src/main/java/rgba/SkillShare/control/AdmController.java
+++ src/SkillShare/src/main/java/rgba/SkillShare/control/AdmController.java
@@ -13,12 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  *  Classe que define os endpoints para adm
@@ -27,40 +32,55 @@ import java.util.Optional;
 @RestController
 @CrossOrigin
 @RequestMapping("/adm")
+@Api("API de administradores")
 public class AdmController {
 
     @Autowired 
     AdmRepository admRepository;
 
     /** 
-    *  Endpoint para cadastro de administrador
+    *  Endpoint para cadastro de administrador.
     * @param adm
-    * @return Retorna para a página definida
     * @author Nicholas Roque
     */
     @PostMapping("/cadastrar")
+    @ApiOperation("Cria um usuário do tipo administrador.")
     @ResponseStatus(HttpStatus.CREATED)
-    public Adm createAdm(@RequestBody Adm adm){
+    @ApiResponse(code = 201,message = "Usuário criado com sucesso.")
+    public Adm createAdm(@RequestBody @ApiParam("Informações do administrador.") Adm adm){
         System.out.println(adm.toString());
         return admRepository.save(adm);
     }
     /** 
-    *  Endpoint para listar todos os administradores
-    * @return Retorna um ModelAndView com a lista de administradores
+    *  Endpoint para listar todos os administradores.
+    * @return Retorna uma lista do objeto Adm com todos os administradores.
     * @author Nicholas Roque
     */
     @GetMapping("/findAll")
+    @ApiOperation("Retorna uma lista com todos os usuários do tipo administrador.")
+    @ApiResponse(code = 200,message = "Usuários retornados com sucesso.")
     public List<Adm> getAllAdm(){
         return admRepository.findAll();
     }
 
+    /** 
+    *  Endpoint para retornar os detalhes de um administrador.
+    * @return Retorna objeto do tipo Adm com os dados do usuário.
+    * @param cpf
+    * @return Adm
+    * @author Nicholas Roque
+    */
     @GetMapping("{cpf}")
-    public ResponseEntity<Optional<Adm>> getAdmByCpf(@PathVariable String cpf) {
-        Optional<Adm> adm = admRepository.findById(cpf);
-            return adm.map(a -> {
-                return new ResponseEntity<Optional<Adm>>(adm, HttpStatus.OK);
-            }).orElseThrow(() -> {
-                return new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuário não encontrado");
-            });
+    @ApiOperation("Retorna os detalhes de um usuário do tipo administrador.")
+    @ApiResponses({
+        @ApiResponse(code = 200,message = "Usuário do tipo aluno encontrado com sucesso."),
+        @ApiResponse(code = 404,message = "Usuário do tipo aluno não encontrado para o cpf informado.")
+    })
+    public Adm getAdmByCpf(@PathVariable @ApiParam("Cpf do administrador.") String cpf) {
+        return admRepository
+            .findById(cpf)
+            .orElseThrow(()->
+                new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuário do tipo aluno não encontrado")
+            );
     }
 }
--- src/SkillShare/src/main/java/rgba/SkillShare/control/AlunoController.java
+++ src/SkillShare/src/main/java/rgba/SkillShare/control/AlunoController.java
@@ -13,13 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-import java.util.Optional;
 
 
 
@@ -32,41 +37,55 @@ import java.util.Optional;
 @RestController
 @CrossOrigin
 @RequestMapping("/aluno")
+@Api("API de aluno")
 public class AlunoController {
 
     @Autowired 
     AlunoRepository aRepository;
 
     /** 
-    *  Endpoint para cadastro de aluno
+    *  Endpoint para cadastro de aluno.
     * @param aluno
-    * @return Retorna para a página definida
     * @author Nicholas Roque
     */
     @PostMapping("/cadastrar")
     @ResponseStatus(HttpStatus.CREATED)
-    public Aluno createAluno(@RequestBody Aluno aluno){
+    @ApiOperation("Cria um usuário do tipo aluno.")
+    public Aluno createAluno(@RequestBody @ApiParam("Informações do aluno") Aluno aluno){
 
         return aRepository.save(aluno);
     }
 
     /** 
-    *  Endpoint para listar todos os alunos
-    * @return Retorna um ModelAndView com a lista de alunos
+    *  Endpoint para listar todos os alunos.
+    * @return Retorna uma lista do objeto Aluno com todos os alunos. 
     * @author Nicholas Roque
     */
     @GetMapping("/findAll")
+    @ApiOperation("Retorna uma lista com todos os usuários do tipo aluno")
+    @ApiResponse(code = 200,message = "Usuários retornados com sucesso.")
     public List<Aluno> getAllAlunos(){
         return aRepository.findAll();
     }
 
+    /** 
+    *  Endpoint para retornar os detalhes de um aluno.
+    * @return Retorna objeto do tipo Aluno com os dados do usuário.
+    * @param cpf
+    * @return Adm
+    * @author Nicholas Roque
+    */
     @GetMapping("{cpf}")
-    public ResponseEntity<Optional<Aluno>> getAlunoByCpf(@PathVariable String cpf) {
-        Optional<Aluno> aluno = aRepository.findById(cpf);
-            return aluno.map(a -> {
-                return new ResponseEntity<Optional<Aluno>>(aluno, HttpStatus.OK);
-            }).orElseThrow(() -> {
-                return new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuário não encontrado");
-            });
+    @ApiOperation("Retorna os detalhes de um usuário do tipo aluno")
+    @ApiResponses({
+        @ApiResponse(code = 200,message = "Usuário do tipo aluno encontrado com sucesso."),
+        @ApiResponse(code = 404,message = "Usuário do tipo aluno não encontrado para o cpf informado.")
+    })
+    public Aluno getAlunoByCpf(@PathVariable @ApiParam("Cpf do aluno") String cpf) {
+        return aRepository
+            .findById(cpf)
+            .orElseThrow(()->
+                new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuário do tipo aluno não encontrado")
+            );
     }
 }
