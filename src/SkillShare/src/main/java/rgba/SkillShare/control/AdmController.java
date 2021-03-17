2a3f6d2fd1073fb1578738653ca37758b3a0d8fa
NicholasRoque <nicholas.sroque@gmail.com>
2021-03-16 23:14:42 -0300

    get adm and aluno by cpf

--- src/SkillShare/src/main/java/rgba/SkillShare/control/AdmController.java
+++ src/SkillShare/src/main/java/rgba/SkillShare/control/AdmController.java
@@ -6,21 +6,19 @@ import rgba.SkillShare.repository.AdmRepository;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
-
+import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-
-
-
-
+import java.util.Optional;
 
 /**
  *  Classe que define os endpoints para adm
@@ -56,4 +54,13 @@ public class AdmController {
         return admRepository.findAll();
     }
 
+    @GetMapping("{cpf}")
+    public ResponseEntity<Optional<Adm>> getAdmByCpf(@PathVariable String cpf) {
+        Optional<Adm> adm = admRepository.findById(cpf);
+            return adm.map(a -> {
+                return new ResponseEntity<Optional<Adm>>(adm, HttpStatus.OK);
+            }).orElseThrow(() -> {
+                return new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuário não encontrado");
+            });
+    }
 }
--- src/SkillShare/src/main/java/rgba/SkillShare/control/AlunoController.java
+++ src/SkillShare/src/main/java/rgba/SkillShare/control/AlunoController.java
@@ -6,16 +6,20 @@ import rgba.SkillShare.repository.AlunoRepository;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 
 
@@ -56,4 +60,13 @@ public class AlunoController {
         return aRepository.findAll();
     }
 
+    @GetMapping("{cpf}")
+    public ResponseEntity<Optional<Aluno>> getAlunoByCpf(@PathVariable String cpf) {
+        Optional<Aluno> aluno = aRepository.findById(cpf);
+            return aluno.map(a -> {
+                return new ResponseEntity<Optional<Aluno>>(aluno, HttpStatus.OK);
+            }).orElseThrow(() -> {
+                return new ResponseStatusException(HttpStatus.NOT_FOUND,"Usuário não encontrado");
+            });
+    }
 }
