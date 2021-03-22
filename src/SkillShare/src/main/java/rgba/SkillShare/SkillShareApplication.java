b3e46e3e93246df511910f269152797bb824c006
Rafael Furtado <rafs.97@hotmail.com>
2021-03-22 08:45:53 -0300

    Update SkillShareApplication.java

--- src/SkillShare/src/main/java/rgba/SkillShare/SkillShareApplication.java
+++ src/SkillShare/src/main/java/rgba/SkillShare/SkillShareApplication.java
@@ -1,16 +1,12 @@
 package rgba.SkillShare;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import rgba.SkillShare.repository.AlunoRepository;
 
 @SpringBootApplication
 public class SkillShareApplication implements CommandLineRunner {
-    @Autowired 
-    AlunoRepository rp;
     
 	public static void main(String[] args) {
 		SpringApplication.run(SkillShareApplication.class, args);
@@ -20,6 +16,5 @@ public class SkillShareApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		//coloque algum codigo que precisa ser executado quando o servidor subir aqui
-
 	}
 }
