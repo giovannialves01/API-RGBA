4371f2190f5560692740cb60d628be010134ee7c
Rafael Furtado <rafs.97@hotmail.com>
2021-04-03 22:37:51 -0300

    Término do footer
    Tempo gasto: 3h

--- src/SkillShare/src/main/resources/static/js/Componentes/Footer.js
+++ src/SkillShare/src/main/resources/static/js/Componentes/Footer.js
@@ -4,55 +4,104 @@ class Footer extends HTMLElement {
     }
 
     connectedCallback() {
-        let footer = document.createElement("div");
-        let logo = document.createElement("div");
-        let opcoes = document.createElement("div");
-        let navegacao = document.createElement("div");
-        let navegacao2 = document.createElement("div");
-
-        let img = document.createElement("img");
-        let label = document.createElement("label");
-
-        let h1 = document.createElement("h1");
-        let p = document.createElement("p");
-        let h12 = document.createElement("h1");
-        let p2 = document.createElement("p");
-
-        footer.classList.add("footer");
-        logo.classList.add("logo");
-        opcoes.classList.add("opcoes");
-        navegacao.classList.add("navegacao");
-        navegacao2.classList.add("navegacao");
-
-        h1.classList.add("title");
-        p.classList.add("text");
-        h12.classList.add("title");
-        p2.classList.add("text");
-
-        h1.textContent = "Navegação";
-        p.textContent = "Home";
-        h12.textContent = "Sobre Nós";
-        p2.textContent = "GitHub";
-
-        img.src = "../static/images/logoSkillShare.PNG";
-        img.alt = "Logo SkillShare";
-
-        label.textContent = "© 2021 - SkillShare - RGBA";
-
-        footer.appendChild(logo);
-        opcoes.appendChild(navegacao);
-        opcoes.appendChild(navegacao2);
-        footer.appendChild(opcoes);
-
-        logo.appendChild(img);
-        logo.appendChild(label);
-
-        navegacao.appendChild(h1);
-        navegacao.appendChild(p);
-        navegacao2.appendChild(h12);
-        navegacao2.appendChild(p2);
+        let footer = this.buildFooter();
 
         this.appendChild(footer);
+    }
+
+    buildSVG(){
+        let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttributeNS(null, "viewBox", "0 0 500 150");
+        svg.setAttributeNS(null, "preserveAspectRatio", "none");
+        svg.style = "height: 100%; width: 100%; position: absolute; z-index: -1;"
+
+        let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+        path.setAttributeNS(null, "d", "M-18.34,23.19 C136.84,-28.13 306.15,103.13 511.00,10.36 L500.00,150.00 L0.00,150.00 Z");
+        path.style="stroke: none; fill: #0AADA7;"
+
+        svg.appendChild(path);
+
+        return svg;
+    }
+
+    buildFooter(){
+        let footer = document.createElement("div");
+        footer.classList.add("footer");
+
+        let logo = this.buildLogo();
+        let options = this.buildOptions();
+        let svg = this.buildSVG();
+
+        footer.appendChild(logo);
+        footer.appendChild(options);
+        footer.appendChild(svg);
+
+        return footer;
+    }
+
+    buildLogo(){
+        let logoContainer = document.createElement("div");
+        logoContainer.classList.add("logo");
+
+        let logo = document.createElement("img");
+        logo.src = "../static/images/logoSkillShare.PNG";
+
+        let copyrigth = document.createElement("label");
+        copyrigth.textContent = "© 2021 - SkillShare - RGBA";
+
+        logoContainer.appendChild(logo);
+        logoContainer.appendChild(copyrigth);
+
+        return logoContainer;
+    }
+
+    buildOptions(){
+        let navegacaoContainer = document.createElement("div");
+        navegacaoContainer.classList.add("opcoes");
+
+        let navegacao = this.buildNavegacao();
+        let sobreNos = this.buildSobreNos();
+
+        navegacaoContainer.appendChild(navegacao);
+        navegacaoContainer.appendChild(sobreNos);
+
+        return navegacaoContainer;
+    }
+
+    buildNavegacao(){
+        let container = document.createElement("div");
+        container.classList.add("navegacao");
+
+        let header = document.createElement("h1");
+        header.classList.add("title");
+        header.textContent = "Navegação";
+
+        let option1 = document.createElement("p");
+        option1.classList.add("text");
+        option1.textContent = "Home";
+
+        container.appendChild(header);
+        container.appendChild(option1);
+
+        return container;
+    }
+
+    buildSobreNos(){
+        let container = document.createElement("div");
+        container.classList.add("navegacao");
+
+        let header = document.createElement("h1");
+        header.classList.add("title");
+        header.textContent = "Sobre nós";
+
+        let option1 = document.createElement("p");
+        option1.classList.add("text");
+        option1.textContent = "GitHub";
+
+        container.appendChild(header);
+        container.appendChild(option1);
+
+        return container;
     }
 }
 
