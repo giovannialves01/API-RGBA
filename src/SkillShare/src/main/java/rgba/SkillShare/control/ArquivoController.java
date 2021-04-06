a7871601db6bfbaa6462aefe4efea91087c55c22
NicholasRoque <nicholas.sroque@gmail.com>
2021-04-06 11:06:51 -0300

    upload

---
+++ src/SkillShare/src/main/java/rgba/SkillShare/control/ArquivoController.java
@@ -0,0 +1,53 @@
+package rgba.SkillShare.control;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import rgba.SkillShare.model.Arquivo;
+
+import rgba.SkillShare.repository.ArquivoRepository;
+
+
+/**
+ * Classe criada com o fim de realizar upload de arquivos
+ * 
+ * @author Nicholas Roque
+ *
+ */
+
+@RestController
+@CrossOrigin
+@RequestMapping("/arquivo")
+@Api("API de usuário")
+public class ArquivoController {
+	
+	@Autowired 
+    ArquivoRepository arquivoRepository;
+
+	/**
+	 * @throws IOException
+	 * 
+	 */
+	@PostMapping("/upload")
+    @ApiOperation("Efetua o upload de um arquivo.")
+    @ResponseStatus(HttpStatus.CREATED)
+	@ApiResponses(value = {@ApiResponse(code = 200, message = "Arquivo postado com sucesso."), @ApiResponse(code = 500, message = "Erro ao postar arquivo.")})
+	public Arquivo uploadArquivo(@RequestParam MultipartFile arquivo) throws IOException {
+		Arquivo arq = new Arquivo(arquivo.getOriginalFilename(),arquivo.getBytes(),arquivo.getContentType());
+		arquivoRepository.save(arq);
+		return arq;
+	}
+}
--- src/SkillShare/src/main/java/rgba/SkillShare/model/Arquivo.java
+++ src/SkillShare/src/main/java/rgba/SkillShare/model/Arquivo.java
@@ -5,17 +5,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 /**
  *  Classe que define o arquivo
  *  @author Nicholas Roque
  */
 @Entity(name="arquivo")
-@NoArgsConstructor @AllArgsConstructor @Data
+@NoArgsConstructor @AllArgsConstructor @Data @ToString
 public class Arquivo {
 
     @Id
@@ -25,8 +27,9 @@ public class Arquivo {
     @Column(nullable = false)
     private String nomeArquivo;
 
+    @Lob
     @Column(nullable = false)
-    private Byte arquivo;
+    private byte[] arquivo;
 
     @Column(nullable = false)
     private String tipoArquivo;
@@ -38,9 +41,13 @@ public class Arquivo {
     * @param tipoArquivo -> tipoArquivo
     * @author Nicholas Roque
     */
-    public Arquivo(String nomeArquivo,Byte arquivo,String tipoArquivo){
+    public Arquivo(String nomeArquivo,byte[] arquivo,String tipoArquivo){
         this.nomeArquivo = nomeArquivo;
         this.arquivo = arquivo;
+        this.tipoArquivo = tipoArquivo;
+    }
+    public Arquivo(String nomeArquivo,String tipoArquivo){
+        this.nomeArquivo = nomeArquivo;
         this.tipoArquivo = tipoArquivo;
     }
 }
