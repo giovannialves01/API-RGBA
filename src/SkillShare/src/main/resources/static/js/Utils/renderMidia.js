6fdf75462a7a602e39e9bc2ec450eb0cb93a8f55
NicholasRoque <nicholas.sroque@gmail.com>
2021-04-07 11:11:51 -0300

    upload e exibição de videos e audios

--- src/SkillShare/src/main/resources/static/js/Utils/renderMidia.js
+++ src/SkillShare/src/main/resources/static/js/Utils/renderMidia.js
@@ -3,6 +3,10 @@
  * @param {String} tipo - tipo do arquivo retornado pelo java
  * @param {String} id - id da tag Img que vai renderizar a imagem
  */
-function renderIMG(arquivo,tipo,id){
+ function renderMidia(arquivo,tipo,id){
     document.getElementById(id).setAttribute("src",`data:${tipo};base64,${arquivo}`)
+    document.getElementById(id).setAttribute("type",`${tipo}`)
+    document.getElementById(id).load()
+
+
 }
