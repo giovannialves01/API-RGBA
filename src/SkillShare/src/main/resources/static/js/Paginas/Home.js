885d4cd414e753882239dfa31847b7d240cea6f5
Rafael Furtado <rafs.97@hotmail.com>
2021-04-08 06:50:50 -0300

    Adicionando responsividade ao slider
    O slider agora se adapta em tempo real ao tamanho disponível da tela
    
    Tempo gasto: 30min

--- src/SkillShare/src/main/resources/static/js/Paginas/Home.js
+++ src/SkillShare/src/main/resources/static/js/Paginas/Home.js
@@ -2,15 +2,15 @@ function sliderController(){
     let slider = document.getElementById("slider");
     
     setInterval(function () {
-        slider.style.backgroundImage = "url(../images/img-1.png)";
+        slider.src = "../images/img-1.png";
     }, 5000);
 
     setInterval(function () {
-        slider.style.backgroundImage = "url(../images/img-2.png)";
+        slider.src = "../images/img-2.png";
     }, 10000);
 
     setInterval(function () {
-        slider.style.backgroundImage = "url(../images/img-3.png)";
+        slider.src = "../images/img-3.png";
     }, 15000);
 }
 
