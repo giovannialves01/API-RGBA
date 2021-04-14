28703718a8f63789a14dc2179fc563e9d396ec41
NicholasRoque <nicholas.sroque@gmail.com>
2021-04-14 10:04:53 -0300

    remoção de imports nao utilizados

--- src/SkillShare/src/main/java/rgba/SkillShare/control/DestaqueController.java
+++ src/SkillShare/src/main/java/rgba/SkillShare/control/DestaqueController.java
@@ -4,14 +4,12 @@ package rgba.SkillShare.control;
 import java.io.IOException;
 import java.util.List;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
