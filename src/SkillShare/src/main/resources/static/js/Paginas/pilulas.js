37d29e474d9daea77c67b799a3ecb4017f5eca59
NicholasRoque <nicholas.sroque@gmail.com>
2021-04-16 19:53:38 -0300

    renderConteudo function and fix accordion

--- src/SkillShare/src/main/resources/static/js/Paginas/pilulas.js
+++ src/SkillShare/src/main/resources/static/js/Paginas/pilulas.js
@@ -2,36 +2,36 @@ window.onload = function carregarConteudo () {
     console.log("carregar Conteudo função");
     serverRequester.fazerGet("/cursos").then((res) => {
         let listagemCursos = document.getElementById("listagem-cursos")
-        let div = ''
+        let div = '<hr>'
         res.responseJson.map((c)=>{
             let imgTeste = "https://img.freepik.com/fotos-gratis/3d-rendem-de-uma-mesa-de-madeira-com-uma-imagem-defocussed-de-um-barco-em-um-lago_1048-3432.jpg?size=626&ext=jpg"
-            let linha = '<div class="linha">'
-            let divCurso = `<button class="accordion"><list-curso curso-id="${c.id}" curso-titulo="${c.titulo}" curso-thumb="${imgTeste}"></list-curso></button>`
-            let divPilulas = '<div class="panel">'
+            let linha =`
+            <div class="linha tab">
+                <input type="checkbox" id="${c.id}">
+                <label class="curso tab-label" for="${c.id}"><list-curso curso-id="${c.id}" curso-titulo="${c.titulo}" curso-thumb="${imgTeste}"></list-curso><i class="fas fa-chevron-left"></i></label>
+                <div class="tab-content">
+                 `
+            let divPilulas ='<hr><br/ >'
             c.pilulas.map((p)=>{
-                divPilulas+=`<list-pilula pilula-id="${p.id}" pilula-titulo ="${p.titulo}" pilula-conteudo ="${p.arquivo.conteudo}" pilula-formato ="${p.arquivo.formato}" pilula-descricao="${p.descricao}" pilula-id-arquivo="${p.arquivo.id}"></list-pilula>`
+                divPilulas+=`
+                <div class="pilula tab">
+                    <input type="checkbox" id="${p.id}">
+                    <label class="pilulaTexto tab-label" for="${p.id}"><i class="fas fa-capsules"></i>${p.titulo}<i class="fas fa-chevron-left"></i></label>
+                    <div class="tab-content">
+                    <div class="conteudoPilula">
+                        ${renderConteudo(p.arquivo.tipoArquivo,p.arquivo.conteudo)}
+                        <p>${p.descricao}</p>
+                    </div>
+                    </div>
+                </div><hr>`
             })
-            divPilulas+='</div>'
-            linha+=divCurso+divPilulas+"</div>"
+            linha+=divPilulas
+            linha+=`</div></div>`
             div+=linha
         })
-
-        /* res.responseJson.map((c)=>{
-            let linha = '<div class="linha">'
-            let imgTeste = "https://img.freepik.com/fotos-gratis/3d-rendem-de-uma-mesa-de-madeira-com-uma-imagem-defocussed-de-um-barco-em-um-lago_1048-3432.jpg?size=626&ext=jpg"
-            let divPilulas = ''
-            c.pilulas.map((p)=>{
-                divPilulas+=`<list-pilula pilula-id="${p.id}" pilula-titulo ="${p.titulo}" pilula-conteudo ="${p.arquivo.conteudo}" pilula-formato ="${p.arquivo.formato}" pilula-descricao="${p.descricao}" pilula-id-arquivo="${p.arquivo.id}"></list-pilula>`
-            })
-            linha += divCurso
-            linha+=divPilulas
-            linha+='</div><br/><br/>'
-            div+=linha
-        }) */
         listagemCursos.innerHTML = div
 
     }).catch((err) => {
         console.log(err);
     }); 
-    collapseAccordion()
 } 
