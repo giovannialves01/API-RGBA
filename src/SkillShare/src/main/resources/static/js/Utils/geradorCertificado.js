dae5a824adb57ae0cf11fa7247d73a6793af7bde
Giovanni Alves <giovanni33316@gmail.com>
2021-05-20 10:01:08 -0300

    certificado já está sendo gerado com a data atual

--- src/SkillShare/src/main/resources/static/js/Utils/geradorCertificado.js
+++ src/SkillShare/src/main/resources/static/js/Utils/geradorCertificado.js
@@ -1,7 +1,25 @@
 function genPDF() {
+
+    now = new Date
+    monName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro");
+
     var el = document.getElementById("certificado");
     var doc = new jsPDF("l", 'mm', 'a4');
     var imgData = 'data:image/png;base64,'+ Base64.encode(el);
     doc.addImage(el, 'PNG', 0, 0, 297, 210);
-    doc.save("Test.pdf");
+    doc.setFontSize(40);
+    doc.addFont('ComicSansMS', 'Comic Sans', 'normal');
+    doc.addFont('ComicSansMS', 'Comic Sans', 'bold');
+    doc.setFont('Comic Sans');
+    doc.setFontType("bold");
+    doc.text(55, 95, 'Giovanni dos Santos Alves,')
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.text(55, 105, 'em ' + now.getDate() + ' de ' + monName [now.getMonth() ] + ' de ' + now.getFullYear() + ', concluiu o curso de')
+    doc.setFontSize(40);
+    doc.setFontType("bold");
+    doc.text(55, 120, 'Network Fundamentals')
+    doc.setFontSize(20);
+    doc.setFontType("normal");
+    doc.save("Certificado.pdf");
 }
