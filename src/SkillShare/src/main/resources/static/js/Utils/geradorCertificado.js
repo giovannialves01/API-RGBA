fbd86ccaffec498477ba13a4f7b099e88c25fe1f
Giovanni Alves <giovanni33316@gmail.com>
2021-05-20 13:07:06 -0300

    A geração de certificado já está bem estruturada e formatada, agora é necessária o link com o banco de dados

--- src/SkillShare/src/main/resources/static/js/Utils/geradorCertificado.js
+++ src/SkillShare/src/main/resources/static/js/Utils/geradorCertificado.js
@@ -5,20 +5,25 @@ function genPDF() {
 
     var el = document.getElementById("certificado");
     var doc = new jsPDF("l", 'mm', 'a4');
-    var imgData = 'data:image/png;base64,'+ Base64.encode(el);
+    var nomeCurso= 'Curso de análise de dados publicos';
+    var nomeCursoFormatado = doc.splitTextToSize(nomeCurso, (70));
+    var nomeAluno = 'Giovanni dos Santos Alves';
+    var tmp = nomeAluno.split(' ');
+    var nomeAlunoFormatado = tmp[0] + ' ' + tmp[tmp.length-1] + ',';
+    
     doc.addImage(el, 'PNG', 0, 0, 297, 210);
     doc.setFontSize(40);
     doc.addFont('ComicSansMS', 'Comic Sans', 'normal');
     doc.addFont('ComicSansMS', 'Comic Sans', 'bold');
     doc.setFont('Comic Sans');
-    doc.setFontType("bold");
-    doc.text(55, 95, 'Giovanni dos Santos Alves,')
+    doc.setFontType("bold");  
+    doc.text(55, 95, nomeAlunoFormatado);
     doc.setFontSize(20);
     doc.setFontType("normal");
-    doc.text(55, 105, 'em ' + now.getDate() + ' de ' + monName [now.getMonth() ] + ' de ' + now.getFullYear() + ', concluiu o curso de')
+    doc.text(55, 105, 'em ' + now.getDate() + ' de ' + monName [now.getMonth() ] + ' de ' + now.getFullYear() + ', concluiu o curso de');
     doc.setFontSize(40);
     doc.setFontType("bold");
-    doc.text(55, 120, 'Network Fundamentals')
+    doc.text(55, 120, nomeCursoFormatado);
     doc.setFontSize(20);
     doc.setFontType("normal");
     doc.save("Certificado.pdf");
