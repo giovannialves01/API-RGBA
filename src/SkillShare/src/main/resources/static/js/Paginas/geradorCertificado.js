189dfd94ed07c31cf1d1e1db44aba4168c678f96
Rafael Furtado <rafs.97@hotmail.com>
2021-05-28 21:15:13 -0300

    Disponibilizando certificado para aluno
    Tempo gasto: 2h

--- src/SkillShare/src/main/resources/static/js/Paginas/geradorCertificado.js
+++ src/SkillShare/src/main/resources/static/js/Paginas/geradorCertificado.js
@@ -1,27 +1,24 @@
 async function loadCertificados() {
     let containerCertificados = document.getElementById("certificadosToShow");
-	containerCertificados.innerHTML = ''
+	
+	let cpfAluno = localStorage.getItem("cpfUser");
 
-    let response = await serverRequester.fazerGet("/alunos"); //aqui
+    let response = await serverRequester.fazerGetWithData("/alunos/certificados", {"cpfAluno": cpfAluno});
 
-    let destaques = response["responseJson"];
-
-    for (let i = 0; i < destaques.length; i++) {
-        const data = destaques[i];
-        
-        //let certificadoo = new Certificado(); //aqui
-
+    let certificados = response["responseJson"];
+	
+    for (let i = 0; i < certificados.length; i++) {
+        const data = certificados[i];
+		
         let entityIdentifier = "certificado" + (i + 1);
 
         let entityContainer = document.createElement("div");
         entityContainer.classList.add("contentBox");
         entityContainer.id = 'certificado ' + (i + 1);
-        let containerCadaCertificados = document.getElementById('certificado ' + (i + 1));
-        
+        let containerCadaCertificados = document.getElementById('certificado-' + (i + 1));       
         
         let showContentLabel = document.createElement("label");
-		showContentLabel.textContent = "aqui vai o nome do curso";
-        showContentLabel.id = 'nomeCurso';
+		showContentLabel.textContent = data["nomeCurso"];
 		
 		let showContentButton = document.createElement("button");
 		showContentButton.textContent = "Baixar Certificado";
@@ -30,29 +27,18 @@ async function loadCertificados() {
                 now = new Date
     			monName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro");
 
-    			var el = document.getElementById("certificado");
     			var doc = new jsPDF("l", 'mm', 'a4');
-    			var nomeCurso= 'Curso de análise de dados publicos';
-    			var nomeCursoFormatado = doc.splitTextToSize(nomeCurso, (70));
+    			var nomeCursoFormatado = doc.splitTextToSize(data["nomeCurso"], (70));
     			var nomeAluno = 'Giovanni dos Santos Alves';
    				var tmp = nomeAluno.split(' ');
     			var nomeAlunoFormatado = tmp[0] + ' ' + tmp[tmp.length-1] + ',';
     
-    			doc.addImage(el, 'PNG', 0, 0, 297, 210);
-    			doc.setFontSize(40);
-    			doc.addFont('ComicSansMS', 'Comic Sans', 'normal');
-    			doc.addFont('ComicSansMS', 'Comic Sans', 'bold');
-    			doc.setFont('Comic Sans');
-    			doc.setFontType("bold");  
-    			doc.text(55, 95, nomeAlunoFormatado);
+				let img = getImageBase64(data["imagemDeFundo"]["conteudo"], data["imagemDeFundo"]["tipoArquivo"]);
+    			doc.addImage(img, 'PNG', 0, 0, 297, 210);
     			doc.setFontSize(20);
-    			doc.setFontType("normal");
-    			doc.text(55, 105, 'em ' + now.getDate() + ' de ' + monName [now.getMonth() ] + ' de ' + now.getFullYear() + ', concluiu o curso de');
-    			doc.setFontSize(40);
-    			doc.setFontType("bold");
-    			doc.text(55, 120, nomeCursoFormatado);
-    			doc.setFontSize(20);
-    			doc.setFontType("normal");
+    			
+    			doc.text(55, 95, data["mensagem"]);
+
     			doc.save("Certificado.pdf");
         }
 		
@@ -62,20 +48,24 @@ async function loadCertificados() {
 		
         let fieldsContainer = document.createElement("div");
         fieldsContainer.style.width = "60%";
-
             
 
-            
+		
 
-            if(i < destaques.length - 1){
-                let separador = document.createElement("div");
-                separador.classList.add("separador");
-    
-                containerCertificados.appendChild(separador);
-            }
+		if(i < certificados.length - 1){
+			let separador = document.createElement("div");
+			separador.classList.add("separador");
+
+			containerCertificados.appendChild(separador);
+		}
 
 
     }
     
 }
+
+function getImageBase64(arquivo, tipo){
+    return "data:" + tipo + ";base64," + arquivo;
+}
+
 loadCertificados();
